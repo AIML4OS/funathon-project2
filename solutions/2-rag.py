46dae3ce4da4a7405348e4cf972e53699276be31
jpramil <julien.pramil@insee.fr>
2026-03-12 13:11:56 +0000

    qdrant con

---
+++ solutions/2-rag.py
@@ -0,0 +1,28 @@
+# %%
+
+# Create Duckdb connection
+import duckdb
+con = duckdb.connect(database=":memory:")
+con.execute("INSTALL httpfs;")
+con.execute("LOAD httpfs;")
+
+# Get data
+path_data = "https://minio.lab.sspcloud.fr/projet-formation/diffusion/funathon/2026/project2/generation_None_temp08.parquet"
+query_definition = f"SELECT * FROM read_parquet('{path_data}')"
+annotations = con.sql(query_definition).to_df()
+annotations.head()
+
+
+# %%
+import os
+from dotenv import load_dotenv
+from qdrant_client import QdrantClient
+
+load_dotenv()
+
+client_qdrant = QdrantClient(
+    url=os.environ["QDRANT_URL"],
+    api_key=os.environ["QDRANT_API_KEY"],
+    port=os.environ["QDRANT_API_PORT"]
+)
+# %%
